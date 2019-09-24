a3f23e50736c4bd8941ea1f57bf9e2d9a63ca3c2
santiagombv <santiagombv@gmail.com>
2019-09-24 17:59:03 -0300

    add app

---
+++ selgradients/app.R
@@ -0,0 +1,43 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    # Application title
+    titlePanel("Gradientes de selección"),
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("b1", label = "beta 1", min=-2, max=2, value = 0, step = 0.1),
+            sliderInput("b2", label = "beta 2", min=-2, max=2, value = 0, step = 0.1), 
+            width=3),
+        
+        mainPanel(
+            webGLOutput("pob1"), width=7)
+    )
+)
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
